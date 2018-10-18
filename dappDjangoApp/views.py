8c515c0096c51caea4ffbba120b2c23d1929989c
rujeetjahagirdar <r1jahagirdar@gmail.com>
2018-10-19 00:00:56 +0530

    Display table account and Balance

--- dappDjangoApp/views.py
+++ dappDjangoApp/views.py
@@ -6,7 +6,10 @@ def main_view(request):
 	return render(request,"main_view.html",{})
 
 def result(request):
-	i=int(request.POST.get('accountIndex'))
+	accountData={}
+	#i=int(request.POST.get('accountIndex'))
 	web3=Web3(HTTPProvider('http://127.0.0.1:7545'))
-	amount=int(web3.fromWei(web3.eth.getBalance(web3.eth.accounts[i-1]),'ether'))
-	return render(request,"result.html",{'ans':amount})
+	for i in web3.eth.accounts:
+		accountData[i]=web3.fromWei(web3.eth.getBalance(i),'ether')
+	#amount=int(web3.fromWei(web3.eth.getBalance(web3.eth.accounts[i-1]),'ether'))
+	return render(request,"result.html",{'ans':accountData})
